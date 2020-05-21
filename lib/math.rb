ac2493ec11a48d951f2f5a43be69f6ad2234707a
Eve Reichmann <ereichmann4@aol.com>
2020-05-21 20:08:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,21 +6,33 @@ def addition(num1,num2)
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  num1 = 10 
+  num2 = 5
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  num1 = 50
+  num2 = 2 
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  num1 = 4
+  num2 = 30
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  num1 = 34
+  num2 = 5
+  sum
 end
 
 def square_root(num)
-
+  sum 
 end
